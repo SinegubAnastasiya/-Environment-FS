e7fc6b9c101b810130fd3c3249fae8715ef157af
SinegubAnastasiya <fedukevich98@mail.ru>
2023-12-13 21:03:14 +0300

    added some remarks

--- index.js
+++ index.js
@@ -7,33 +7,54 @@ app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
-    const data = getAllData()
-    res.send(data)
+    try {
+        const data = getAllData()
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+
 })
 
 app.get('/:id', (req, res) => {
-    const { id } = req.params
-    const data = getDataById(id)
-    res.send(data)
+    try {
+        const { id } = req.params
+        const data = getDataById(id)
+        res.send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 })
 
 app.post('/', (req, res) => {
-    const { label, category, priority} = req.body
-    const data = addData(label, category, priority)
-    res.send(data)
+    try {
+        const { label, category, priority } = req.body
+        const data = addData(label, category, priority)
+        res.send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 })
 
 app.put('/:id', (req, res) => {
-    const { id } = req.params
-    const { label, category, priority } = req.body
-    const data = updateData(id, label, category, priority)
-    res.send(data)
+    try {
+        const { id } = req.params
+        const { label, category, priority } = req.body
+        const data = updateData(id, label, category, priority)
+        res.send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 })
 
 app.delete('/:id', (req, res) => {
-    const { id } = req.params
-    const data = deleteData(id)
-    res.send(data)
+    try {
+        const { id } = req.params
+        const data = deleteData(id)
+        res.send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 })
 
 app.listen(3000, () => console.log('Server is running'))
--- service.js
+++ service.js
@@ -3,11 +3,13 @@ const json = fs.readFileSync('./storage.json')
 const arr = JSON.parse(json)
 
 function getAllData() {
+    if (!arr.length) throw new Error('Array is empty')
     return arr
 }
 
 function getDataById(id) {
     const data = arr.filter(el => el.id == id)
+    if (!data.length) throw new Error('This id not found')
     return data
 }
 
@@ -32,14 +34,14 @@ function updateData(id, label, category, priority) {
     }
     const data = arr.findIndex(el => el.id == id)
     arr[data] = newObject
-
+    if (!data.length) throw new Error ('this id not found')
     fs.writeFileSync('./storage.json', JSON.stringify(arr))
     return arr
 }
 
 function deleteData(id) {
     const newArr = arr.filter(el => el.id !== id) 
-
+    if (arr.length == newArr.length) throw new Error ('this id not found')
     fs.writeFileSync('./storage.json', JSON.stringify(arr))
     return newArr
 }
